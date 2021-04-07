2bab1c225cbbf62c71a5040f73341d42565e2ba2
Giovanni/Desktop <uomoermeneutico@gmail.com>
2021-04-07 21:09:57 +0200

    added comment to the interface

--- projects/ng-jvx-multiselect/src/lib/interfaces/ng-jvx-option-mapper.ts
+++ projects/ng-jvx-multiselect/src/lib/interfaces/ng-jvx-option-mapper.ts
@@ -1,5 +1,8 @@
 import {Observable, Subject} from 'rxjs';
 
+/**
+ * Maps the option returned by the async call in an object of type T. When object is mapped the resulting object is returned in an observable
+ */
 export interface NgJvxOptionMapper<T> {
   mapOption(source: any): Observable<T>;
 }
