32045abaa2a13e4e264c429f8ee55f82c5f1b0f2
Giovanni Venturelli <giovanni.venturelli@digitronica.it>
2023-11-14 11:24:20 +0100

    tentativo di risolvere un problema su 'no value accessor'

--- projects/ng-jvx-multiselect/src/lib/ng-jvx-option/ng-jvx-option.component.ts
+++ projects/ng-jvx-multiselect/src/lib/ng-jvx-option/ng-jvx-option.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewChecked, Component, ContentChild, DoCheck, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, DoCheck, Input, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {MatListOption} from '@angular/material/list';
 
 @Component({
@@ -6,6 +6,7 @@ import {MatListOption} from '@angular/material/list';
   selector: 'ng-jvx-option',
   templateUrl: './ng-jvx-option.component.html',
   styleUrls: ['./ng-jvx-option.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NgJvxOptionComponent implements OnInit, DoCheck {
   @ViewChild('listOption', {static: true}) listOption: MatListOption;
